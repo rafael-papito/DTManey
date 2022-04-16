a0bf59a2b299a8a4ca3bd930871a5b80214888ad
Rafael Porto <73806403+rafael-papito@users.noreply.github.com>
2022-04-15 22:03:39 -0300

    fializando o dia! Falta terminar o os botões de enserer e retirar dos modais e a parte mais lógica da coisa, o react em si

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal';
 import {Header} from './components/Header';
 import { Deshboard } from './components/Deshboard';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
 export function App() {
+  const [isNewTransctionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() { 
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Deshboard/>
+      <NewTransactionModal
+        isOpen={isNewTransctionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle/>
     </>
   );
